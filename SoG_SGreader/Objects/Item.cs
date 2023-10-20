82f2b908bcdbd3dd0139ceb1d4d14f87895fde53
Anatolij Vasilev <Anatolij.Vasilev@flyeralarm.com>
2023-10-20 22:22:02 +0200

    added more tests, added tets.yml mock up

--- SoG_SGreader/Objects/Item.cs
+++ SoG_SGreader/Objects/Item.cs
@@ -4,15 +4,25 @@ namespace SoG_SGreader
 {
     public class Item
     {
+        public SogItems ItemID { get; set; }
+        public int ItemCount { get; set; }
+        public UInt32 ItemPos { get; set; }
         public Item(SogItems _ItemID, int _ItemCount, UInt32 _ItemPos)
         {
             this.ItemID = _ItemID;
             this.ItemCount = _ItemCount;
             this.ItemPos = _ItemPos;
         }
-        public SogItems ItemID { get; set; }
-        public int ItemCount { get; set; }
-        public UInt32 ItemPos { get; set; }
+        
+        public override bool Equals(object obj)
+        {
+            Item item = obj as Item;
+            if (item == null)
+            {
+                return false;
+            }
 
+            return this.ItemID == item.ItemID && this.ItemCount == item.ItemCount && this.ItemPos == item.ItemPos;
+        }
     }
 }
